9d24ad71d3d55135d0776ede3df2632dc887bafa
BOUYAKNIFEN Zakariae <30890032+zakariabouy@users.noreply.github.com>
2026-08-20 15:08:03 +0100

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.py
@@ -0,0 +1,16 @@
+# 65-90 
+# 97-122
+# 48-57
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.lower()
+        n=len(s)
+        k=''
+        for i in range(n):
+            if (ord(s[i])>=97 and ord(s[i])<=122) or (ord(s[i])>=48 and ord(s[i])<=57):
+                k+=s[i]
+        m=len(k)
+        for i in range(m//2):
+            if k[i]!=k[-i-1]:
+                return False
+        return True
